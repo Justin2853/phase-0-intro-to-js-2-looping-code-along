19ea399e8a1d9793cb945e3320c01b668a25a3ee
Justin <justin2853@gmail.com>
2021-09-15 18:25:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = ["Ada", "Brendan", "Ali"];
+
+function writeCards(names, surprise) {
+    let writtenLetters = [];
+    for(let i = 0; i < names.length; i++){
+    writtenLetters.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return writtenLetters;
+}
+function countDown(startingNumber){
+    let userNumber = startingNumber;
+    while (0 <= startingNumber){
+    console.log(startingNumber --);
+    }
+}
+countDown(10);
